c393b6d58ae9b3aa7cd5295c9d6212d0b3b600b0
Creighton Gorai <creighton-gorai@users.noreply.github.com>
2023-10-16 20:45:48 -1000

    Adding feature to save a sav of all the missing states

--- main.py
+++ main.py
@@ -8,29 +8,34 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 screen.setup(width=850, height=525)
 turtle.shape(image)
-
 data = pandas.read_csv("50_states.csv")
 
-correct_guesses = 0
+correct_guesses = []
 
-is_game_on = True
-while is_game_on:
+while correct_guesses != 50:
 
-    answer = screen.textinput(title=f"{correct_guesses}/50 Guess the State", prompt="What's another states name?")
-    print(answer)
-    caps_answer = answer.title()
-    print(caps_answer)
+    answer = screen.textinput(title=f"{len(correct_guesses)}/50 Guess the State",
+                              prompt="What's another states name?").title()
 
     for state in data["state"]:
-        if state == caps_answer:
+        if state == answer:
             print_state = PrintState()
+            correct_guesses.append(state)
             x_coor = data[data["state"] == state]["x"]
             x_coor_item = x_coor.tolist()
             y_coor = data[data["state"] == state]["y"]
             y_coor_item = y_coor.tolist()
             print_state.print_the_state(state, x_coor_item[0], y_coor_item[0])
-            correct_guesses += 1
             break
+    if answer == "Exit":
+        missing_states = []
+        for state in data.state.to_list():
+            if state not in correct_guesses:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
 
 
 
